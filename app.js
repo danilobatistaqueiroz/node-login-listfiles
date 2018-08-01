b28e4f77b1ffa79b7dfb852df71a78c536121fc9
Danilo Batista Queiroz <danilobatistaqueiroz@gmail.com>
2018-07-31 23:30:16 -0300

    login and list only private

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var app = express();
+var fs = require('fs');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var express = require('express'),
+  app = express(),
+  port = process.env.PORT || 3000;
 
-//... your code here ...
-                                
-var port = process.env.PORT || 3000;
-app.listen(port, function () {
-    console.log('Umbler listening on port %s', port);
-});
+app.listen(port);
+
+app.use(cookieParser());
+app.use(session({ secret: 'example' }));
+app.use(bodyParser());
+app.set('view engine', 'ejs');
+
+	
+console.log('List of files server started on: ' + port);
+
+require('./routes.js')(app);
